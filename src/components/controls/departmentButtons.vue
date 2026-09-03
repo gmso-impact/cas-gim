b39b77f38718d86690065da7da5044444d8d39f6
ibisprojects <webmaster@citsci.org>
2026-09-02 20:02:57 -0600

    new buttons

--- src/components/controls/departmentButtons.vue
+++ src/components/controls/departmentButtons.vue
@@ -13,71 +13,73 @@
       <div class="btn-group" role="group" aria-label="Basic example">
         <button
           type="button"
-          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-2 mr-2"
+          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-1 mr-2"
           :class="
             isActive('Agricultural Biology')
-              ? 'btn-primary active'
+              ? 'btn-primary agBio'
               : 'btn-outline-secondary'
           "
           @click="setDepartment('Agricultural Biology')"
         >
-          <img height="100" src="icons/2-1.png" alt="Agricultural Biology" />
+          <img height="80" src="icons/2-1.png" alt="Agricultural Biology" title="Agricultural Biology" />
         </button>
         <button
           type="button"
-          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-2 mr-2"
+          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-1 mr-2"
           :class="
             isActive('Agricultural and Resource Economics')
-              ? 'btn-primary active'
+              ? 'btn-primary agResEcon'
               : 'btn-outline-secondary'
           "
           @click="setDepartment('Agricultural and Resource Economics')"
         >
           <img
-            height="100"
+            height="80"
             src="icons/5-1.png"
             alt="Agricultural and Resource Economics"
+            title="Agricultural and Resource Economics"
           />
         </button>
         <button
           type="button"
-          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-2 mr-2"
+          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-1 mr-2"
           :class="
             isActive('Animal Sciences')
-              ? 'btn-primary active'
+              ? 'btn-primary animalSciences'
               : 'btn-outline-secondary'
           "
           @click="setDepartment('Animal Sciences')"
         >
-          <img height="100" src="icons/3-1.png" alt="Animal Sciences" />
+          <img height="80" src="icons/3-1.png" alt="Animal Sciences" title="Animal Sciences" />
         </button>
         <button
           type="button"
-          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-2 mr-2"
+          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-1 mr-2"
           :class="
             isActive('Horticulture and Landscape Architecture')
-              ? 'btn-primary active'
+              ? 'btn-primary hortLandArch'
               : 'btn-outline-secondary'
           "
           @click="setDepartment('Horticulture and Landscape Architecture')"
         >
           <img
-            height="100"
+            height="80"
             src="icons/4-1.png"
             alt="Horticulture and Landscape Architecture"
+            title="Horticulture and Landscape Architecture"
           />
         </button>
         <button
           type="button"
-          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-2 mr-2"
+          class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center p-1 mr-2"
           :class="
             isActive('Soil and Crop Sciences')
-              ? 'btn-primary active'
+              ? 'btn-primary soilCropSciences'
               : 'btn-outline-secondary'
           "
           @click="setDepartment('Soil and Crop Sciences')"
         >
-          <img height="100" src="icons/5-2.png" alt="Soil and Crop Sciences" />
+          <img height="80" src="icons/5-2.png" alt="Soil and Crop Sciences" title="Soil and Crop Sciences" />
         </button>
         <!-- `btn-${story.fields['Department']}` :class="[`bg-${theme}`]"-->
       </div>
@@ -108,13 +110,36 @@ export default {
 }
 
 .circle-icon {
-  background: #59595b;
+  background: #323E48;
   width: 38px;
   height: 38px;
   border-radius: 50%;
   text-align: center;
   line-height: 100px;
   vertical-align: middle;
-  padding: 10px;
+  padding: 16px;
 }
+
+.agBio {
+  background-color: #323E48 !important;
+  border: solid 1px #feb015 !important;
+}
+
+.agResEcon {
+  background-color: #323E48 !important;
+  border: solid 1px #2E953E !important;
+}
+.animalSciences {
+  background-color: #323E48 !important;
+  border: solid 1px #00AEEF !important;
+}
+.hortLandArch {
+  background-color: #323E48 !important;
+  border: solid 1px #F15625 !important;
+}
+.soilCropSciences {
+  background-color: #323E48 !important;
+  border: solid 1px #82C341 !important;
+}
+
 </style>
