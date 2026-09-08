e1f3e7c1c9695a3d3cfbe6feed6ff8a6f115881b
Brandon J Budnicki <50023776+BBudnicki@users.noreply.github.com>
2026-09-08 09:34:26 -0600

    Adopt impact.agsci domain
    Adopt impact.agsci domain

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/cas-gim/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   devServer: {
     host: "0.0.0.0",
     port: 8080,
