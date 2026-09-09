4b0db7e94ce48722af6ba01c93574c860c1f933e
ibisprojects <webmaster@citsci.org>
2026-09-09 11:24:42 -0600

    style changes

--- src/components/overlay/filter.vue
+++ src/components/overlay/filter.vue
@@ -1,6 +1,5 @@
 <template lang="">
   <div class="overlay childPoint" v-if="isFilterFrameOpen">
-    <!-- isFilterFrameOpen overlay childPoint -->
     <div class="controlFrame px-1 py-1 rounded bg-carbon border border-slate">
       <div class="overflow-y-auto overflow-x-hidden pb-5 h-100 w-100">
         <h1 class="text-white text-center mx-1 pt-5">
@@ -8,9 +7,6 @@
         </h1>
         <DepartmentButtons class="mb-3 mx-1"></DepartmentButtons>
         <TypeButtons class="mb-3 mx-1"></TypeButtons>
-        <!-- <CampusGroup class="mb-3 mx-1"></CampusGroup>
-        <ThemeColumn class="mb-3 mx-1"></ThemeColumn> -->
-        <!-- <IdTags class="mb-3 mx-1"></IdTags>-->
       </div>
       <div class="topClose">
         <button
@@ -37,17 +33,11 @@
 </template>
 <script>
 import { mapGetters, mapMutations } from "vuex";
-//import CampusGroup from "@/components/controls/campusGroup.vue";
-//import ThemeColumn from "@/components/controls/themeColumn.vue";
-//import IdTags from "@/components/controls/idTags.vue";
 import DepartmentButtons from "@/components/controls/departmentButtons.vue";
 import TypeButtons from "@/components/controls/typeButtons.vue";
 
 export default {
   components: {
-    //CampusGroup,
-    //ThemeColumn,
-    //IdTags,
     DepartmentButtons,
     TypeButtons
   },
